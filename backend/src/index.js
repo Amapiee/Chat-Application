50bbb5dcc0c1bd4ab2cf27aa794628dee4e17df9
Nhật Huy Trần <nhathuyhp0609@gmail.com>
2026-04-28 23:28:52 +0700

    Fixed typo again

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,7 +40,7 @@ app.use(cookieParser()); // Để đọc được cookie từ request
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-if(process.env.Node_ENV === "production"){
+if(process.env.NODE_ENV === "production"){
     app.use(express.static(path.join(__dirname, "frontend/dist")));
 
     app.get("*", (req, res) => {
